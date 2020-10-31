a4eb73cb465d9f04fb742025b196bf8b2dd70837
chanhaPrk <qkrcksgk90@gmail.com>
2020-10-31 21:29:11 +0900

    add useInput.

--- src/App.js
+++ src/App.js
@@ -2,47 +2,22 @@
 import React, {useState} from "react";
 import ReactDom from "react-dom";
 
+const useInput = (initialValue) => {
+    const [value, setValue] = useState( initialValue );
+    const onChange = (event) => {
+        console.log(event.target);
+    }
+    return {value,onChange};
+};
+
 function App() {
-  const [item,setItem] = useState(100);
-  const incrementItem = () => setItem( item + 1);
-  const decrementItem = () => setItem( item - 1);
+  const name = useInput("Mr.");
   return <div>
-      <h1>react hooks useState.</h1>
-      <div>item number is : {item}</div>
-      <div>item number is : {setItem}</div>
-      <button onClick={incrementItem}>incrementItem</button>
-      <button onClick={decrementItem}>decrementItem</button>
+      <h1>react hooks useInput.</h1>
+      <input placeholder="Name" {...name} />
+      //<input placeholder="Name" value={name.value}/>
+      //<input placeholder="Name" value={name.value} onChange={name.onChange} />
       </div>
-}
-
-
-//위 방식을 class component 방식으로 똑같이 만들어보자.! 
-class uglyApp extends React.Component{
-    state = {
-      item : 1
-    };
-    render(){
-        return <div>
-        <h1>react hooks useState.</h1>
-        <div>item number is : {this.state.item}</div>
-        <button onClick={this.incrementItem}>incrementItem</button>
-        <button onClick={this.decrementItem}>decrementItem</button>
-        </div>
-    };
-    incrementItem = () => {
-       this.setState( state => {
-           return {
-               item : state.item +1
-           }
-       })
-   }
-    decrementItem = () => {
-       this.setState( state => {
-           return {
-               item : state.item -1
-           }
-       })
-   }
 }
 
 export default App;
