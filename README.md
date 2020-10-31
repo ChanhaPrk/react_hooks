6c01ae135141d36561433d8cb1242c89ddb4bbc3
chanhaPrk <qkrcksgk90@gmail.com>
2020-10-31 20:33:24 +0900

    add useState library.

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
 ## what is a react hooks??
+### hooks의 사용 목적
+1 hooks는 component의 state machine에 연결하는 기본적인 방법이다.
+2 class component보다는 function component을 권장한다.
 
-test
+## library
+### useState
+useState 라이브러리를 사용하기 위해선 다음과 같이 import해야만 합니다.
+<code>
+import React {setState} from "react";
+</code>
+
+다음은 useState 라이브러리를 사용하였을때 state의 데이터를 변경하는 예제소스 입니다.
+<code>
+    function ThisisFunctionComponent(){
+      const [item,setItem] = useState(100);
+      incrementItem = () => setState(item+1);
+      decrementItem = () => setState(item-1);
+      return <div>
+          <h1>react hooks useState.</h1>
+          <div>item number is : {item}</div>
+          <button onClick={incrementItem}>incrementItem</button>
+          <button onClick={decrementItem}>decrementItem</button>
+          </div>
+    }
+</code>
+<code>
+    const [item,setItem] = useState(100);    
+</code>
+위 코드에서 item에는 useState()함수으로 주입한 데이터가 세팅됩니다. 그렇다면 setItem은 무엇인가요?
+<code>
+    incrementItem = () => setState(item+1);
+</code>
+위 코드와 같이 state의 데이터를 변경하고자할때 호출할 수 있는 함수입니다.        
+즉 useState()함수는 세팅하고자하는 데이티와 상태를 변경할 수 있는 함수을 리턴하게 됩니다.
--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+//import React from "react";
+import React, {useState} from "react";
+import ReactDom from "react-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [item,setItem] = useState(100);
+  const incrementItem = () => setItem( item + 1);
+  const decrementItem = () => setItem( item - 1);
+  return <div>
+      <h1>react hooks useState.</h1>
+      <div>item number is : {item}</div>
+      <div>item number is : {setItem}</div>
+      <button onClick={incrementItem}>incrementItem</button>
+      <button onClick={decrementItem}>decrementItem</button>
+      </div>
+}
+
+
+//위 방식을 class component 방식으로 똑같이 만들어보자.! 
+class uglyApp extends React.Component{
+    state = {
+      item : 1
+    };
+    render(){
+        return <div>
+        <h1>react hooks useState.</h1>
+        <div>item number is : {this.state.item}</div>
+        <button onClick={this.incrementItem}>incrementItem</button>
+        <button onClick={this.decrementItem}>decrementItem</button>
+        </div>
+    };
+    incrementItem = () => {
+       this.setState( state => {
+           return {
+               item : state.item +1
+           }
+       })
+   }
+    decrementItem = () => {
+       this.setState( state => {
+           return {
+               item : state.item -1
+           }
+       })
+   }
 }
 
 export default App;
